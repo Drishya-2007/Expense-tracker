ef160bdadc863328a05538a48564d57d04b33ebb
Drishya-2007 <drishya3507@gmail.com>
2026-01-31 12:41:38 +0530

    Serve frontend and rename landing page

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -6,9 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Connect to MongoDB
 
-mongoose.connect("mongodb://127.0.0.1:27017/expenseTracker")
+mongoose.connect(process.env.MONGO_URI)
+
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
@@ -51,8 +56,10 @@ app.delete("/expenses/:id", async (req, res) => {
 });
 
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
